00ef8025482b2b4c65866cd204c2b569c2921ad7
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-08-03 21:14:04 -0300

    sobe api

---
+++ api/relatorioacessivel/src/main/java/br/com/martinsgms/relatorioacessivel/RelatorioacessivelApplication.java
@@ -0,0 +1,13 @@
+package br.com.martinsgms.relatorioacessivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioacessivelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatorioacessivelApplication.class, args);
+	}
+
+}
---
+++ api/relatorioacessivel/src/test/java/br/com/martinsgms/relatorioacessivel/RelatorioacessivelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.martinsgms.relatorioacessivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatorioacessivelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
