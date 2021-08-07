d2ff4e92f7d276540638eeaa6c45eb6a1159b9dc
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-08-07 20:47:20 -0300

    adiciona navegação entre exame e relatório

--- android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/DetalheExameActivity.kt
+++ android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/DetalheExameActivity.kt
@@ -1,6 +1,8 @@
 package br.com.martinsgms.relatorioacessivel
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class DetalheExameActivity : AppCompatActivity(R.layout.activity_detalhe_exame) {
@@ -10,6 +12,12 @@ class DetalheExameActivity : AppCompatActivity(R.layout.activity_detalhe_exame) 
 
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val btnRelatorio = findViewById<Button>(R.id.activity_detalhe_atividade_relatorio)
+        btnRelatorio.setOnClickListener() {
+            val intent = Intent(this, RelatorioActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
--- android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/HomeActivity.kt
+++ android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/HomeActivity.kt
@@ -1,11 +1,18 @@
 package br.com.martinsgms.relatorioacessivel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val cardExame = findViewById<CardView>(R.id.card_exame_mais_recente)
+        cardExame.setOnClickListener() {
+            val intent = Intent(this, DetalheExameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
