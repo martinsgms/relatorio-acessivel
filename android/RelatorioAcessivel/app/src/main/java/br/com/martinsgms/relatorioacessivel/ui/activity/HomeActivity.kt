8ecbb550531101751050d6a6a50428f826ce00a7
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-09-02 17:19:03 -0300

    adiciona rest client no app android

--- android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/ui/activity/HomeActivity.kt
+++ android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/ui/activity/HomeActivity.kt
@@ -3,12 +3,23 @@ package br.com.martinsgms.relatorioacessivel.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.cardview.widget.CardView
 import br.com.martinsgms.relatorioacessivel.R
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result;
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        "http://192.168.15.157:8080/exame/usuario/1?apenasMaisRecente=true".httpGet()
+            .responseString { request, response, result ->
+                Log.d("gms-data", result.get())
+            }
+
+        Log.d("gms-data", "fez")
+
 
         val cardExame = findViewById<CardView>(R.id.card_exame_mais_recente)
         cardExame.setOnClickListener() {
