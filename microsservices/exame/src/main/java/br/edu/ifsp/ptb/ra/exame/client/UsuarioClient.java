051b483d69a558cc57fdda62426b420aa848f7b5
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-09-02 21:25:15 -0300

    altera request mapping para getmapping

--- microsservices/exame/src/main/java/br/edu/ifsp/ptb/ra/exame/client/UsuarioClient.java
+++ microsservices/exame/src/main/java/br/edu/ifsp/ptb/ra/exame/client/UsuarioClient.java
@@ -2,14 +2,14 @@ package br.edu.ifsp.ptb.ra.exame.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.edu.ifsp.ptb.ra.exame.dto.UsuarioDTO;
 
 @FeignClient("app-usuario")
 public interface UsuarioClient
 {
-    @RequestMapping("/usuario/{email}")
+    @GetMapping("/usuario/{email}")
     ResponseEntity<UsuarioDTO> buscaUsuarioPorEmail(@PathVariable String email);
 }
