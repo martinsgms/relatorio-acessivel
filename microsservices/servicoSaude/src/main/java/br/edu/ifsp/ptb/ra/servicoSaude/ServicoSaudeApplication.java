d6d4d65f62f2e1b1c0b14f38abc2a720dda22d73
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-09-27 21:43:53 -0300

    cria ms para serviço de saúde

---
+++ microsservices/servicoSaude/src/main/java/br/edu/ifsp/ptb/ra/servicoSaude/ServicoSaudeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ptb.ra.servicoSaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoSaudeApplication.class, args);
+	}
+
+}
---
+++ microsservices/servicoSaude/src/test/java/br/edu/ifsp/ptb/ra/servicoSaude/ServicoSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ptb.ra.servicoSaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
