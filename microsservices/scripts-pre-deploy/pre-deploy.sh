9de95278e7578986d9c7efdce69c11b458ead0b7
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-10-13 16:42:39 -0300

    fixes nos scripts

--- microsservices/scripts-pre-deploy/pre-deploy.sh
+++ microsservices/scripts-pre-deploy/pre-deploy.sh
@@ -5,16 +5,16 @@ start_time="$(date -u +%s)"
 printf "\nremovendo imagens...\n"
 docker rmi $(docker images -q martinsgms/ra-app-usuario)
 docker rmi $(docker images -q martinsgms/ra-app-exame)
-docker rmi $(docker images -q martinsgms/ra-app-servicoSaude)
+docker rmi $(docker images -q martinsgms/ra-app-servicosaude)
 docker rmi $(docker images -q martinsgms/ra-gateway-server)
 docker rmi $(docker images -q martinsgms/ra-config-server)
-docker rmi $(docker images -q martinsgms/ra-eureka-serve)
+docker rmi $(docker images -q martinsgms/ra-eureka-server)
 
 printf "\npacking...\n"
 ./generate-all-packages.sh
 
 printf "\nbuilding imagens...\n"
-docker-compose build
+docker-compose build --no-cache
 
 printf "\nuploading to DockerHub...\n"
 docker-compose push
