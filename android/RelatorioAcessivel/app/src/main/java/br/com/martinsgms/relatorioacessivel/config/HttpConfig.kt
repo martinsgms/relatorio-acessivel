9ffbd7e0386e7c9349f1a9f022c2fbb3a16a6fac
Gabriel Martins dos Santos <martins.gabriel@aluno.ifsp.edu.br>
2021-11-17 00:05:29 -0300

    atualiza setup do ec2 e basepath do app

--- android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/config/HttpConfig.kt
+++ android/RelatorioAcessivel/app/src/main/java/br/com/martinsgms/relatorioacessivel/config/HttpConfig.kt
@@ -8,8 +8,12 @@ class HttpConfig {
     companion object Factory {
         var token = ""
         var userId = 0L
-        var BASE_PATH_LOCAL = "http://172.18.41.49"
-        var BASE_PATH_CLOUD = "http://ec2-44-195-80-58.compute-1.amazonaws.com:32168"
+
+        // local
+        //var BASE_PATH = "http://172.18.41.49"
+
+        // aws
+        var BASE_PATH = "http://34.236.191.183:31255"
 
         var BASE_HEADERS = mapOf("Content-Type" to "application/json")
 
@@ -17,7 +21,7 @@ class HttpConfig {
 
             BASE_HEADERS = mapOf("Content-Type" to "application/json", "Authorization" to "Bearer $token")
             Log.d("headddd", userId.toString())
-            FuelManager.instance.basePath = BASE_PATH_LOCAL
+            FuelManager.instance.basePath = BASE_PATH
             FuelManager.instance.baseHeaders = BASE_HEADERS
         }
 
--- aws/ec2-setup.sh
+++ aws/ec2-setup.sh
@@ -1,24 +1,27 @@
-#!/bin/bash
+#!/bin/nash
 
-# (manualmente) alterar senha do root
-# sudo passwd root
-
-# instala kubernetes
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl
-
-# dá permissão de execução kubernetes e move pro path
-chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-
-# atualiza pacotes
 sudo apt-get update
+sudo apt-get upgrade
 
-# instala docker. Importante ser a versão abaixo
-sudo apt-get install docker.io=17.12.1-0ubuntu1
+# install kubernetes
+curl -LO https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo kubectl version --client
 
-# instala minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.32.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin && rm minikube
+# install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
-# (manualmente) ativa minikube
-# sudo -su root
-# minikube start --vm-driver=none
-# minikube status
+# install docker
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# install conntrack
+sudo apt install conntrack
+
+# launch minikube
+sudo minikube start --vm-driver=none
+sudo minikube status
